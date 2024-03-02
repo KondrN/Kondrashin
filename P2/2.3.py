e44fc33f87ca0cc813c1a1d9ed42a4d0bc2d29e5
KondrN <nikitakondrasin17ssau@gmail.com>
2024-03-02 14:44:00 +0400

    second commit

--- P2/2.3.py
+++ P2/2.3.py
@@ -1,8 +1,19 @@
+# importing libraries
+from mpl_toolkits import mplot3d
+import numpy as np
 import matplotlib.pyplot as plt
-import  numpy as np
-theta = np.linspace(0, 90,150)
-r= theta/72
-plt.figure(figsize=(6, 6))
-plt.axes(projection='polar')
-plt.scatter(theta, r,s=400*r**2,c=theta,cmap='hsv',alpha=0.8)
+
+# defining surface and axes
+x = np.outer(np.linspace(0, 100, 1000), np.ones(10))
+y = np.outer(np.linspace(0, 100, 1000), np.ones(10))
+z = x**4+y**4
+
+fig = plt.figure()
+
+# syntax for 3-D plotting
+ax = plt.axes(projection='3d'),
+
+# syntax for plotting
+ax.plot_surface(x, y, z, cmap='viridis',edgecolor='green')
+ax.set_title('Surface plot geeks for geeks')
 plt.show()
