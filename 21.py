11b3765709bf34767eb3899a6ff7e5fc379be33f
KondrN <nikitakondrasin17ssau@gmail.com>
2024-03-02 12:02:40 +0400

    second commit

--- 21.py
+++
@@ -1,6 +0,0 @@
-x = [1, 3, 4, 7, 8, 9, 10]
-razn=0
-for i in range(0,len(x)-1):
-    if(x[i+1]-x[i]>razn):
-        razn = x[i+1]-x[i]
-print(razn/0.01)
--- 6.py
+++
@@ -1,3 +0,0 @@
-s = "change me"
-print(s)
-print(s.replace("e", "E"))
