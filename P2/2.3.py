8e126383a68ff9bb7ad08869294f11c7af72dd03
KondrN <nikitakondrasin17ssau@gmail.com>
2024-03-02 13:58:07 +0400

    second commit

--- P2/2.3.py
+++ P2/2.3.py
@@ -1,13 +1,8 @@
 import matplotlib.pyplot as plt
 import  numpy as np
-theta = np.linspace(0, 360, 360)
-r= theta/6
+theta = np.linspace(0, 90,150)
+r= theta/72
 plt.figure(figsize=(6, 6))
 plt.axes(projection='polar')
-plt.scatter(theta, r)
-plt.scatter(theta, r,
- s=400*r**2,
- c=theta,
- cmap='hsv',
- alpha=0.8
- )
+plt.scatter(theta, r,s=400*r**2,c=theta,cmap='hsv',alpha=0.8)
+plt.show()
